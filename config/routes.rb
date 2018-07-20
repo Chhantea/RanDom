db7dee8645ad6d1e4f9e5e6ddc0eabce24139895
Chhantea <r.lalnghenchhana@gmail.com>
2018-07-20 13:51:12 +0530

    remove remote true call, fixed css

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "dashboard#index"
   match ':controller(/:action(/:id))', :via => [:get, :post]
-  get "dashboard/product/:id", to: "dashboard#product", as: :dashboard_product
+  get "dashboard/product/:id :name", to: "dashboard#product", as: :dashboard_product
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
